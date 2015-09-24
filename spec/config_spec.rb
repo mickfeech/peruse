bf3c4fa34f78a5e47024b7938c6042a49eaeee3b
mcfeech <mcfeech@keybank.com>
2015-09-24 11:57:35 -0400

    remove whitespace

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -3,5 +3,9 @@ require 'peruse'
 describe Peruse::Config do
   it 'should be configurable' do
     expect(Peruse.config.auth_token).to be(nil)
+    Peruse.configure do |config|
+      config.auth_token = 'token-string'
+    end
+    expect(Peruse.config.auth_token).to eq('token-string')
   end
 end
