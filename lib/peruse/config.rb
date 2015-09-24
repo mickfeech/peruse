317211cf546a2093d025000bc37fd565ccaad722
MCFEECH <mcfeech@keybank.com>
2015-09-24 15:21:47 -0400

    moving auth up to the module.

--- lib/peruse/config.rb
+++ lib/peruse/config.rb
@@ -2,10 +2,15 @@ module Peruse
   class Config
     attr_accessor :auth_token, :username, :password
 
-    def initialize
-      @auth_token = nil
-      @username = nil
-      @password = nil
+    def initialize(options={})
+      @username  ||= options[:username]
+      @password  ||= options[:password]
+      @auth_token ||= nil
+    end
+
+    private
+    def get_token
+
     end
   end
 end
