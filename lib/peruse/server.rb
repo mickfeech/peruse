912958ba954eb75faa8f14204f1b08f83f70bea7
mcfeech <mcfeech@keybank.com>
2015-09-24 10:43:03 -0400

    update docs

--- lib/peruse/server.rb
+++ lib/peruse/server.rb
@@ -1,5 +1,7 @@
 module Peruse
   # @author mickfeech
+  # = Server
+  # The server class provides the framework for you to connect to a specified Plex Media Server
   class Server
     attr_reader :host, :port
 
@@ -8,7 +10,7 @@ module Peruse
       @port = port
     end
 
-    # Returns the URL endpoint to the plex media server
+    # Returns the URL endpoint to the Plex Media Server
     def url
       "http://#{host}:#{port}"
     end
