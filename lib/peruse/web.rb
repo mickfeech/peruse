14bfd8898e166935bab7bac26b0e38228426fe43
MCFEECH <mcfeech@keybank.com>
2015-09-24 20:12:24 -0400

    documentation

--- lib/peruse/web.rb
+++ lib/peruse/web.rb
@@ -3,11 +3,13 @@ module Peruse
   # = Web
   # The Web class provides the interface with plex.tv
   class Web
+    attr_reader :username, :password
 
     # @param [username] your plex.tv username
     # @param [passowrd] your plex.tv password
     def initialize(username, password)
-
+      @username = username
+      @password = password
     end
   end
 end
